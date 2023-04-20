efc8e67a960fe4a91553185db18f5971e8fe01d6
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-20 11:39:25 -0300

    generate list of new users

---
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Main from "../template/Main";
+import axios from 'axios'
+
+const headerProps = {
+    icon: 'users',
+    title: 'Usuario',
+    subtitle: 'Cadastro de usuario:Incluir, Listar, Alterar e excluir '
+}
+
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
+export default class UserCrud extends Component {
+
+    state = { ...initialState }
+
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list })
+            })
+    }
+    getUpdatedList(user, add = true) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+         list.unshift(user)
+        return list
+    }
+
+    render() {
+
+        return (
+            <Main{...headerProps}>
+                Cadastro de usuario
+            </Main>
+        )
+    }
+}
